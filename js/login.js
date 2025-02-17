6f2922db157a03cc6b54d880eda67171ebaf3971
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2025-02-17 13:02:25 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const password = document.getElementById('password').value;
 
     try {
-        const response = await axios.post('http://localhost:3000/login', { email, password });
+        const response = await axios.post('https://proyecto-notificaciones.onrender.com/login', { email, password });
         alert('Inicio de sesión exitoso');
         // Redireccionar a la página principal
         window.location.href = 'index.html';
